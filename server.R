0e463c616bc033ab66aa73ef6aa436feac83cae8
deepanshur3006 <deepanshu.rustagi88@gmail.com>
2020-08-09 19:14:46 +0530

    Updates to HTML, PDF, Rmd, ui.R and server.R

--- server.R
+++ server.R
@@ -1,63 +1,38 @@
-
-
-
 library(webshot)
-    library(shiny)
-    shinyServer(function(input, output) {
+library(shiny)
+library(dplyr)
+data(mtcars)
+shinyServer(function(input, output) {
+    
+    
+    filterdata<- reactive({
         
-   
-    xlab<-'Random Numbers X Axis'
-    ylab<-'Random Numbers Y Axis'
-    main<-'Plot of Y and X Random Numbers'
+        tempdf<- filter(mtcars,cyl==input$cyl)
         
+    }) 
+    
+    
     output$plot1 <-  renderPlot({
-          
-              number_of_points <- input$numeric
-              minX <- input$sliderX[1]
-              maxX <- input$sliderX[2]
-              minY <- input$sliderY[1]
-              maxY <- input$sliderY[2]
-              
-              
-              dataX <- runif(number_of_points, minX, maxX)
-              dataY <- runif(number_of_points, minY, maxY)
-              datadf<-data.frame(dataX,dataY)
-         
-              plot(datadf$dataX, datadf$dataY, xlab = xlab, ylab = ylab, main = main
-                   ,  xlim = c(-100, 100), ylim = c(-100, 100))
-              
-              if(!is.null(model())){
-                  abline(model(), col = "blue", lwd = 2)
-                    }
-         
-        })
         
-       model <- reactive({
-          
-            brushed_data <- brushedPoints(datadf,input$brush1,
-                                          xvar = "dataX", yvar = "dataY")
-            if(nrow(brushed_data) < 2){
-                return(NULL)
-            }
-            lm(dataY~dataX, data = brushed_data)
-        })
-       
         
-        output$slopeOut <- renderText({
-            if(is.null(model())){
-                "No Model Found"
-            } else {
-                model()[[1]][2]
-            }
-        })
-        
-        output$intOut <- renderText({
-            if(is.null(model())){
-                "No Model Found"
-            } else {
-                model()[[1]][1]
-            }
-        })
-        
+        plot(filterdata()$mpg,xlab = 'MPG Values', ylab = 'Indexes', 
+        main = 'MPG of Cars Having Selected
+                   Number of Cylinders')
         
     })
+    
+    output$maxhp <- renderText({
+        
+        max(filterdata()$hp) 
+        
+    })
+    
+    output$maxwt <- renderText({
+        max(filterdata()$wt)
+        
+    })
+    
+    
+})
+
+
--- ui.R
+++ ui.R
@@ -1,30 +1,25 @@
-
-
 library(webshot)
 library(shiny)
+library(dplyr)
+data(mtcars)
 shinyUI(fluidPage(
-    titlePanel("Interactive Shiny Application"),
+    titlePanel("Interesting mtcars Analysis"),
     sidebarLayout(
         sidebarPanel(
-            h3('Choose how many random numbers you want plotted and their ranges.'),
-            numericInput("numeric", "How Many Random Numbers Should be Plotted? (Max 100)", 
-                         value = 20, min = 20, max = 100, step = 1),
-            sliderInput("sliderX", "Pick Minimum and Maximum X Values",
-                        -100, 100, value = c(-50, 50)),
-            sliderInput("sliderY", "Pick Minimum and Maximum Y Values",
-                        -100, 100, value = c(-50, 50)),
+            h3('Choose the number of cylinders'),
+            radioButtons("cyl", "Number of Cylinders", 
+                         choices = c(4,6,8))
+            
             
         ),
         mainPanel(
-            h3('Plot output after calculation in Server. 
-               You can select certain ponts with cursor. Go on!'),
-            plotOutput("plot1",brush = brushOpts(
-                id = "brush1")),
-            h3('Slope and Intercept of the fitted line for the selected points.'),
-            h3("Slope"),
-            textOutput("slopeOut"),
-            h3("Intercept"),
-            textOutput("intOut"),
+            h3('Here is some trivia!'),
+            h3('MPG Values For Selected Cars'),
+            plotOutput("plot1"),
+            h3('Maximum Horse Power'),
+            textOutput("maxhp"),
+            h3("Maximum Weight"),
+            textOutput("maxwt"),
             h3('Documentation for using app: https://rpubs.com/deepanshu_rustagi88/647435')
         )
     )
